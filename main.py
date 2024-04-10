1b13cdeaffcc0118960fe37215041a50ff368daa
allysei <ally.seiverd@ufl.edu>
2024-04-10 10:17:20 -0400

    created main function that includes the menu, and added choices 1(encode) and 3(quit)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def encoder(password):
+def encoder(password):        #Ally Seiverd
     new_password=""
     for char in password:
         x=int(char)+3
@@ -6,6 +6,21 @@ def encoder(password):
 
     return new_password
 
+
+
+def main():
+    while True:
+        print("Menu","\n-------------","\n1. Encode","\n2. Decode","\n3. Quit")
+        choice=int(input("\nPlease enter an option: "))
+        if choice==1:
+            password=input("Please enter your password to encode: ")
+            encoded=encoder(password)
+            print("Your password has been encoded and stored!")
+            print()
+        if choice==3:
+            break
+if __name__=="__main__":
+    main()
 
 
 
